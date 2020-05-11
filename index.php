44bee09a5c7d43389087f5acb9237f7af039cec8
Zach Hogarth <zach@thedisquisitive.com>
2020-05-11 14:10:21 -0400

    Added index.php. Zeroed debug timers.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'include/head.php';
+include_once 'mysql.php';
+
+echo "Loading... please wait...<br>\n";
+
+header( "refresh:0;url=/wimp/fullinventory.php");
+
+include 'include/foot.php';
+
+?> 
--- submit_edit.php
+++ submit_edit.php
@@ -24,7 +24,7 @@ echo "Query: " . $qry . "<br>\n";
 
 $result = $mysqli->query($qry);
 
-header( "refresh:5;url=/wimp/fullinventory.php");
+header( "refresh:0;url=/wimp/fullinventory.php");
 
 echo "<p>MySQL Result: " .  $result . "<br>\nPage will redirect automatically.<br>\n";
 
